b6fa9eea28e9e0050f239cc0824054e02be077cd
Jane Sebastian <jane@betatetra.com>
2016-08-29 10:07:12 -0700

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,20 @@
+require_relative 'boot'
+require 'rails/all'
+Bundler.require(*Rails.groups)
+module Terraform
+  class Application < Rails::Application
+    config.assets.quiet = true
+    config.generators do |generate|
+      generate.helper false
+      generate.javascript_engine false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+    config.action_controller.action_on_unpermitted_parameters = :raise
+    config.active_job.queue_adapter = :delayed_job
+    config.autoload_paths << Rails.root.join('lib')
+  end
+end
